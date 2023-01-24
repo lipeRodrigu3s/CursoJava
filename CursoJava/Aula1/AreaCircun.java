64ba5ee6664459bd645c6ea0b6750d8925eeee8a
Fellipe Augusto <Fellipe246@hotmail.com>
2023-01-24 08:51:27 -0300

    CursoJava

---
+++ CursoJava/Aula1/AreaCircun.java
@@ -0,0 +1,12 @@
+package Aula1;
+
+public class AreaCircun{
+    public static void main(String[] args) {
+        int raio = 23;
+        final double pi = 3.1415;
+        double areaT = pi * (raio*raio);
+
+        System.out.println(areaT);
+
+    }
+}
