940baeb9a85af5642b879116441c630d075b80d7
Fellipe Augusto <Fellipe246@hotmail.com>
2023-01-24 09:36:55 -0300

    CursoJava

---
+++ CursoJava/Aula1/TiposPrimitivos.java
@@ -0,0 +1,26 @@
+package Aula1;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        
+        //tipos númericos inteiros
+        byte f = 127;//Além de 127 não pode ser byte
+        short d = 5700;  
+        int id = 597412369;
+        long op = 8454782135498756351L;//Para grandes quantidadades precisa colocar o "L" no final
+
+        //Tipos númericos reais
+        float s = 14_569.987F;//Precisa colocar o "F" no final
+        double v = 1575_896547.6315_8888888;
+
+        //Tipos primitivos lógicos
+        boolean t = true;
+        boolean h = false;
+
+        //Tipos primitivos char
+        char l = 'F';
+        String ç = "fellipe";
+
+
+    }
+}
