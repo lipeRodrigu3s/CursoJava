6a88a3ed089c2b3a59c97b096883cd1416d384cd
Fellipe Augusto <Fellipe246@hotmail.com>
2023-01-24 08:31:25 -0300

    CursoJava

---
+++ CursoJava/src/Desafios/Temperatura.java
@@ -0,0 +1,11 @@
+package Desafios;
+
+public class Temperatura {
+    public static void main(String[] args) {
+        int F = 85;
+        double temp = (F -32) * 5/9;
+
+        System.out.println(temp);
+
+    }
+}
